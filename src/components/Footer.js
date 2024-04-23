69a38fabb0629b8edf86fc8fb4a83f855bba4d66
aditi519 <aditipawar510@gmail.com>
2024-04-23 17:02:13 +0530

    feature1 commiting

---
+++ src/components/Footer.js
@@ -0,0 +1,72 @@
+import "./FooterStyles.css";
+
+import React from "react";
+
+import {
+  FaHome,
+  FaPhone,
+  FaMailBulk,
+  FaFacebook,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
+          <div className="location">
+            <FaHome size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+            <div>
+              <p>Karve Nagar</p>
+              <p>Pune, Maharashtra</p>
+            </div>
+          </div>
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              9657835855
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              pawaraditi510@gmail.com
+            </h4>
+          </div>
+        </div>
+
+        <div className="right">
+          <h4>About Myself</h4>
+          <p>
+            This is Aditi Pawar. I enjoy discussing new projects and tackling
+            development challenges.
+          </p>
+          <div className="social">
+            <FaFacebook
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+            <FaTwitter
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+            <FaLinkedin
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/WorkCardData.js
@@ -0,0 +1,31 @@
+import pro1 from "../assets/pro1.jpeg";
+import pro2 from "../assets/pro2.jpeg";
+import pro3 from "../assets/pro3.jpeg";
+
+const ProjectCardData = [
+  {
+    imgsrc: pro1,
+    title: "HelloMaid",
+    text: "The HelloMaid project is aimed at creating an efficient and user-friendly Online Maid Booking System. This web application serves as a comprehensive solution for both users and maids, eliminating the need for physical searches, recommendations, or personal requests. Users can easily find and book maids through a single online platform, while maids can secure work opportunities based on their skills and availability, all accessible through the internet. The primary goal of this project is to deliver a seamless and convenient user experience, coupled with top-notch service provision.",
+    view: "https://github.com/aditi519/HelloMaid",
+    source: "https://github.com/aditi519/HelloMaid",
+  },
+
+  {
+    imgsrc: pro2,
+    title: "Employee Details Management",
+    text: "The Employee Details Management focuses on creating the basic system for managing employee information. The project employs C# and the ASP.NET MVC framework to implement a simple Employee class with attributes like Name, City, State, and Salary. The code utilizes data annotations for validation, ensuring that essential fields are not left blank, and the data entered adheres to the required format. This project serves as a foundation for more comprehensive employee management systems, enabling users to store, retrieve, and validate employee details efficiently.",
+    view: "https://github.com/aditi519/Employee_Details_Management",
+    source: "https://github.com/aditi519/Employee_Details_Management",
+  },
+
+  {
+    imgsrc: pro3,
+    title: "Portfolio Website Design ",
+    text: "Developed an interactive and responsive portfolio using React, showcasing a collection of projects and personal information. Designed the layout and user interface for optimal visual appeal and functionality, Implemented features to display projects dynamically and ensure seamless navigation. Deployed the portfolio to a live environment, demonstrating proficiency in front-end development, React.js, and responsive web design.",
+    view: "https://github.com/aditi519/Portfolio",
+    source: "https://github.com/aditi519/Portfolio",
+  },
+];
+
+export default ProjectCardData;
