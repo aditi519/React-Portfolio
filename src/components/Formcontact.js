f4c06de2ff1d27487f949f653af04242deeaf03b
aditi519 <aditipawar510@gmail.com>
2024-04-25 19:43:13 +0530

    changes

--- src/components/Formcontact.js
+++ src/components/Formcontact.js
@@ -1,18 +1,36 @@
 import "./FormStyles.css";
-
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Formcontact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_2s2x3m8", "template_vyzho55", form.current,"ioRX5kb1YSFO9uYed",
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
   return (
     <div className="form">
-      <form>
-        <label>Your Name</label>
-        <input type="text"></input>
-        <label>Email</label>
-        <input type="email"></input>
-        <label>Message</label>
-        <textarea rows="6" placeholder="Type your message here" />
-        <button className="btn">Submit</button>
+      <form ref={form} onSubmit={sendEmail}>
+        <label htmlFor="user_name">Name</label>
+        <input type="text" id="user_name" name="user_name" />
+        <label htmlFor="user_email">Email</label>
+        <input type="email" id="user_email" name="user_email" />
+        <label htmlFor="message">Message</label>
+        <textarea id="message" name="message" rows="6" placeholder="Type your message here" />
+        <button className="btn" type="submit">Submit</button>
       </form>
     </div>
   );
